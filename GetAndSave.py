21ac9a50237f183ccf95b0a9b6b654cc6cb7cbdb
goodjohn <goodjohn@users.noreply.github.com>
2017-01-01 23:36:56 -0500

    Getting HTTP Status and Headers from urlopen.

--- GetAndSave.py
+++ GetAndSave.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3.4
 
 from urllib.request import urlopen
+from pprint import pprint
 
 example_site = 'http://books.toscrape.com/catalogue/category/books/travel_2/index.html'
+example_pic  = 'http://books.toscrape.com/media/cache/da/a0/daa08c54a927c27494ea5bb90af79c60.jpg'
 
-print(urlopen(example_site).read().decode())
+page = urlopen(example_site)
 
+pprint(page.status)
+pprint(dict(page.info()))
+
+print()
+
+pic = urlopen(example_pic)
+
+pprint(pic.status)
+pprint(dict(pic.info()))
+
+print()
