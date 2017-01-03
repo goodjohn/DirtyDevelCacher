7788f9f15b35e848ac6ea68adf905d9ee520be00
goodjohn <goodjohn@users.noreply.github.com>
2017-01-02 23:06:58 -0500

    Handling exceptions on bad urls (404, 403). Minor refactor.

--- GetAndSave.py
+++ GetAndSave.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3.4
 
+import urllib
 from urllib.request import urlopen
 from pprint import pprint
 
-example_site = 'http://books.toscrape.com/catalogue/category/books/travel_2/index.html'
-example_pic  = 'http://books.toscrape.com/media/cache/da/a0/daa08c54a927c27494ea5bb90af79c60.jpg'
+urls = {
+    'example_site': 'http://books.toscrape.com/catalogue/category/books/travel_2/index.html',
+    'example_pic': 'http://books.toscrape.com/media/cache/da/a0/daa08c54a927c27494ea5bb90af79c60.jpg',
+    'example_404': 'http://books.toscrape.com/this-url-doesnt-exist',
+    'example_403': 'http://books.toscrape.com/media/'
+}
 
-page = urlopen(example_site)
-
-pprint(page.status)
-pprint(dict(page.info()))
-
-print()
-
-pic = urlopen(example_pic)
-
-pprint(pic.status)
-pprint(dict(pic.info()))
-
-print()
+for desc, url in urls.items():
+    print()
+    print(desc)
+    print(url)
+    try:
+        response = urlopen(url)
+        pprint('Response Code: {}'.format(response.status))
+        pprint(dict(response.info()))
+    except urllib.error.HTTPError as e:
+        pprint('Failed: {}'.format(e))
+    print()
