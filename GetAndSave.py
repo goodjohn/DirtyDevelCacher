8f5117fa9092854ef5f2229d8db39b8c86a9c518
goodjohn <goodjohn@users.noreply.github.com>
2017-01-03 23:40:22 -0500

    Saving HTTP Header info to file (no checks for existence/cache yet).

--- GetAndSave.py
+++ GetAndSave.py
@@ -3,6 +3,9 @@
 import urllib
 from urllib.request import urlopen
 from pprint import pprint
+import json
+import os
+from hashlib import md5
 
 urls = {
     'example_site': 'http://books.toscrape.com/catalogue/category/books/travel_2/index.html',
@@ -11,6 +14,11 @@ urls = {
     'example_403': 'http://books.toscrape.com/media/',
     'bad_url':  'http://books.to_scrape.com'
 }
+
+d = 'cache_files'
+
+if not os.path.exists(d):
+    os.makedirs(d)
 
 for desc, url in urls.items():
     print()
@@ -27,6 +35,11 @@ for desc, url in urls.items():
         url_header['status'] = e.status
     except urllib.error.URLError as e:
         url_header['net_conn'] = False
-        url_header['reason'] = e.reason
+        # url_header['reason'] = e.reason  # <class 'socket.gaierror'> is not JSON serializable
     print()
     pprint(url_header)
+    # fn = hash(url)  # Inconsistent output?
+    fn = md5(str(url).encode('utf-8')).hexdigest()
+    # print(fn)
+    with open(d + '/' + str(fn) + '.headers', 'w') as header_cache_file:
+        json.dump(url_header, header_cache_file)
