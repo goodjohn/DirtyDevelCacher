48a5c630866a00230804c3539305a5f887057b07
goodjohn <goodjohn@users.noreply.github.com>
2017-01-07 23:54:26 -0500

    Added more verbose debugging output.

--- GetAndSave.py
+++ GetAndSave.py
@@ -29,6 +29,7 @@ def get_filename(path, name, ext):
 
 
 def fetch_from_web(url):
+    print('[Web]   ', end='')
     content_cache_filename = get_filename(d, url, '.content')
     header_cache_filename = get_filename(d, url, '.header')
     url_header = {}
@@ -40,11 +41,11 @@ def fetch_from_web(url):
     except urllib.error.HTTPError as e:
         url_header['net_conn'] = True
         url_header['status'] = e.status
-        print('Failed: bad HTTP status {}.'.format(e.status))
+        print('Failed: Bad HTTP status {}.'.format(e.status))
     except urllib.error.URLError as e:
         url_header['net_conn'] = False
         url_header['reason'] = '{}'.format(e.reason)
-        print('Failed: bad network connection; {}'.format(e.reason))
+        print('Failed: Bad network connection; {}.'.format(e.reason))
     else:
         url_header['url'] = response.geturl()  # In case of redirects
         with open(content_cache_filename, 'wb') as content_cache_file:
@@ -55,27 +56,36 @@ def fetch_from_web(url):
 
 
 def fetch_from_cache(url, max_age=180):
+    print('[Cache] ', end='')
     content_cache_filename = get_filename(d, url, '.content')
     header_cache_filename = get_filename(d, url, '.header')
     if os.path.isfile(header_cache_filename) and time.time() - os.path.getmtime(header_cache_filename) < max_age:
         with open(header_cache_filename, 'r') as header_file:
             header_info = json.load(header_file)
         if not header_info['net_conn']:
-            print('Bad network connection; resource not in cache')
+            print('Failed: Bad network connection; resource not in cache')
         elif header_info['status'] == 404 or header_info['status'] == 403:
-            print('HTTP Status {}; resource not in cache.'.format(header_info['status']))
+            print('Failed: HTTP Status {}; resource not in cache.'.format(header_info['status']))
         elif os.path.isfile(content_cache_filename):
             with open(content_cache_filename, 'rb') as content_cache_file:
                 # print(content_cache_file.read())
-                print('Content available.')
+                print('Success: Content available.')
             return True
+    else:
+        if not os.path.isfile(header_cache_filename):
+            print('Failed: File not found.')
+        elif time.time() - os.path.getmtime(header_cache_filename) < max_age:
+            print('Failed: Resource exceeds maximum age.')
     return False
 
 
 for desc, url in urls.items():
-    print('{:<12} : '.format(desc), end='')
+    print('_'*80)
+    print(' ', desc)
     if not fetch_from_cache(url):
         fetch_from_web(url)
+    print('\u203e'*80)
+    print()
 
 
 
